6cf1323b09546c731c51cf7848fb6f00177b1ffa
fredtavares2018 <nordesttino@hotmail.com>
2021-07-18 19:13:54 -0300

    subindo...

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule, ErrorHandler, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    AboutPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    BarcodeScanner,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,69 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  public opcoes: any;
+  public dadosLidos: any;
+  public codigo: any;
+  public formato: any;
+  public retorno: any;
+  todo = {}
+
+  constructor(
+    public navCtrl: NavController,
+    private barcode: BarcodeScanner,
+    public http: HttpClient
+    ) {
+
+  }
+
+
+  logForm() {
+    // retorna o que vem do formulário
+    console.log(this.todo)
+    this.http.post("http://localhost/cadastros/cadastros.php", this.todo)
+    .subscribe(data => {
+      //console.log(data['message']);
+      if(data['message'] == 1){
+        this.retorno = "Tudo Certinho!";
+      }else{
+        this.retorno = "Tente Novamente!";
+      }
+     }, error => {
+      console.log(error);
+    });
+  }
+
+  Encode(){
+      var TextToEncode = window.prompt("Escreva aqui");
+      this.barcode.encode(this.barcode.Encode.TEXT_TYPE,TextToEncode)
+      .then((data)=>{
+        alert(JSON.stringify(data));
+      },(err)=>{
+        alert(JSON.stringify(err));
+      }
+
+      )
+  }
+
+  Scan(){
+    this.dadosLidos = null;
+      this.barcode.scan().then((barcodeData)=>{
+        //alert(barcodeData.text);
+        this.dadosLidos = barcodeData;
+          this.codigo = barcodeData.text;
+          this.formato = barcodeData.format;
+      },(err)=>{
+        alert(JSON.stringify(err));
+    })
+  }
+
+}
